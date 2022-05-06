5feb0ff4b1fa4710ed54ee4a660a1c3ae3c0e82b
Nick Story <storynickolasl@gmail.com>
2022-05-06 07:45:08 -0400

    commit tree 0fa41aa1fc9735993b5cf2846328f575b3385f97

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+function findMatching(driverList, driverName) {
+  let driverMatches = []
+  const caps = driverName.toUpperCase()
+  driverList.filter(function (driver) {
+    if(driver.toUpperCase() === caps) {
+      driverMatches.push(driver)
+    };
+  })
+    return driverMatches
+}
+
+function fuzzyMatch(driverList, startLetter) {
+  let fuzzyMatches = []
+  const capLetter = startLetter.toUpperCase()
+  driverList.filter(function (driver) {
+    if(driver.toUpperCase().indexOf(capLetter) === 0) {
+      fuzzyMatches.push(driver)
+    };
+  })
+    return fuzzyMatches
+}
+
+
+function matchName(driverList, driverName) {
+  let matchedNames = []
+  driverList.filter(function (driver) {
+    if(driver.name === driverName) {
+      matchedNames.push(driver)
+    };
+  })
+    return matchedNames
+}
